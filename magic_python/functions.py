43281b4b3820f046692d983b394ef95b9ee7c810
Justin M. Frazier <jfrazier@jmfrazier.org>
2019-12-25 10:10:26 -0500

    Adding appropriate comments

--- magic_python/functions.py
+++ magic_python/functions.py
@@ -1,5 +1,14 @@
 def length(obj: object):
+	"""Replace the 'len' function. What does 'len' actually mean, anyway?
+	* lenny? Is there a 'squiggy' function, too?
+	* lenovo?
+	* less energetic nuetron?"""
 	return len(obj)
 
 def string(obj: object):
+	"""Replace the 'str' function. What does 'str' actually mean?
+	* strong?
+	* store?
+	* strip?
+	* so to recap?"""
 	return str(obj)
--- magic_python/strings.py
+++ magic_python/strings.py
@@ -1,5 +1,5 @@
-
 class file:
+	"""Constants that can be used for file IO operations and keep you from having to search for the right syntax"""
 	WRITE = 'w'
 	READ_ONLY = 'r'
 	APPEND = 'a'
